19ee48451a72cc72631030415cad2b2c93f70cf1
Aditya Barve <aditya2barve@gmail.com>
2021-12-11 19:34:23 -0800

    rename

--- lossy.py
+++ lossy.py
@@ -3,17 +3,17 @@ haystack = "1234567890"
 
 # 251 is largest prime < 256
 # using mod 251 arithmetic, we can keep our hash values in 8 bits
-class Lossy:
+class LossyTenHash:
     @staticmethod
     def FromString(str):
-        h = Lossy()
+        h = LossyTenHash()
         for c in str:
             h = h.add_right(c)
         return h
 
     @staticmethod
     def FromValue(value):
-        return Lossy(value)
+        return LossyTenHash(value)
 
     def __init__(self, value=0):
         self.value = value
@@ -47,10 +47,10 @@ class Lossy:
         return self
 
 
-assert Lossy().value == 0
-assert Lossy.FromString("345").value == 345
-assert Lossy.FromValue(345).value == 345
-assert Lossy.FromValue(345).slide_right("6").value == 456
-assert Lossy.FromValue(345).slide_right("6").add_right("7").value == 4567
-assert Lossy.FromValue(345).slide_left("2").value == 234
-assert Lossy.FromValue(345).slide_left("2").add_left("1").value == 1234
+assert LossyTenHash().value == 0
+assert LossyTenHash.FromString("345").value == 345
+assert LossyTenHash.FromValue(345).value == 345
+assert LossyTenHash.FromValue(345).slide_right("6").value == 456
+assert LossyTenHash.FromValue(345).slide_right("6").add_right("7").value == 4567
+assert LossyTenHash.FromValue(345).slide_left("2").value == 234
+assert LossyTenHash.FromValue(345).slide_left("2").add_left("1").value == 1234
